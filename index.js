6254c171949a3e8c714933a446ac70f156bab431
KU Lok Sun <sundaykuloksun@gmail.com>
2018-09-06 14:55:34 +0800

    Add simple search POC
    Testing typo tolerance in Chinese.

--- index.js
+++ index.js
@@ -21,3 +21,41 @@ const addObjects = () => {
     }
   });
 }
+
+const searchAsync = (query) => {
+  return index.search({
+    query
+    },
+    function searchDone(err, content) {
+      if (err) {
+        console.error(err);
+      } else {
+        const keywords = _.map(content.hits, 'keyword');
+        if (keywords.length > 0) {
+          console.log(`${query}: ${keywords.join(' ')}`);
+        } else {
+          console.log(`${query}: NULL`);
+        }
+      }
+    }
+  );
+}
+
+const main = async () => {
+  await searchAsync('一花生');
+  await searchAsync('一生');
+  await searchAsync('生花');
+  await searchAsync('升派');
+  await searchAsync('花生一地');
+  await searchAsync('局及');
+  await searchAsync('事務局及');
+}
+
+main();
+// 生花: NULL
+// 一花生: 一地花生
+// 事務局及: 政制及內地事務局
+// 一生: 三宅一生 高橋一生
+// 升派: 升小派位 升中派位 直升機派錢
+// 局及: 食物及衞生局 創新及科技局 運輸及房屋局 勞工及福利局 財經事務及庫務局 商務及經濟發展局 政制及內地事務局 美國國家海洋及大氣管理局
+// 花生一地: 一地花生
