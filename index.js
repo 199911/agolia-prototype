cf5865f27d021990f8c33869f7ec5197c4dfa99e
KU Lok Sun <sundaykuloksun@gmail.com>
2018-09-06 11:23:48 +0800

    Set up test data

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 require('dotenv').config();
+const algoliasearch = require('algoliasearch');
+const _ = require('lodash');
 
-console.log(process.env.ALGOLIA_ID);
-console.log(process.env.ALGOLIA_KEY);
+const { ALGOLIA_ID, ALGOLIA_KEY, ALGOLIA_INDEX } = process.env;
+
+const client = algoliasearch(ALGOLIA_ID, ALGOLIA_KEY);
+const index = client.initIndex(ALGOLIA_INDEX);
+
+const keywords = require('./keywords.js');
+const data = _.map(keywords, (keyword) => ({
+  keyword
+}));
+console.log(data);
